1d280ba434b628b0fc388c00ce95a5f5f962108b
ErlHes <erlend.Hestvik@gmail.com>
2020-03-03 17:18:18 +0100

    implemented readgyro_calc
    needs more work.

--- ADCS_Bachelor/ADCS_Bachelor/main.c
+++ ADCS_Bachelor/ADCS_Bachelor/main.c
@@ -30,14 +30,18 @@ int main(void)
 	initMag();
 	initGyro();
 	calibrateMag(1); 
+	int64_t test = calibrateGyro();
+	int16_t gxBias = (test >> 32);
+	int16_t gyBias = (test >> 16);
+	int16_t gzBias = test & 0xFF;
 	while(1){
 
 		int16_t temp = 0;
-		temp = readGyro(OUT_X_L_G);
+		temp = readGyro_calc(OUT_X_L_G, gxBias);
 		gx = calcGyro(temp);
-		temp = readGyro(OUT_Y_L_G);
+		temp = readGyro_calc(OUT_Y_L_G, gyBias);
 		gy = calcGyro(temp);
-		temp = readGyro(OUT_Z_L_G);
+		temp = readGyro_calc(OUT_Z_L_G, gzBias);
 		gz = calcGyro(temp);
 				
 		temp = readMag(OUT_X_L_M);
